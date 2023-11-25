403aea5f32026a0de9cf12d72e7008c7cfaf0d81
Gadzrux <krishnavaghosh23@gmail.com>
2023-11-25 21:15:03 +0530

    passing items & functions via props

--- gamex/src/components/ListGroup.tsx
+++ gamex/src/components/ListGroup.tsx
@@ -1,12 +1,17 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 
-function ListGroup() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+interface Props {
+  items: string[];
+  heading: string;
+  // (item: string) => void
+  onSelectItem: (item: string) => void;
+}
+function ListGroup({ items, heading, onSelectItem }: Props) {
   // Hook
   const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
-    <Fragment>
-      <h1>Lists</h1>
+    <>
+      <h1>{heading}</h1>
       {/* {items.length === 0 ? <p>No item found</p> : null} */}
       {/* when two values are compared like this the second value is returned from
       and (test in console) */}
@@ -22,13 +27,14 @@ function ListGroup() {
             key={item}
             onClick={() => {
               setSelectedIndex(index);
+              onSelectItem(item);
             }}
           >
             {item}
           </li>
         ))}
       </ul>
-    </Fragment>
+    </>
   );
 }
 
